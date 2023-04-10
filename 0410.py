e35fe9e3cd7f49760d288bf10953ce379e1cb2a3
Luna Su <cherry36727179@gmail.com>
2023-04-10 22:33:39 +0800

    add ipynb file

--- 0410.py
+++ 0410.py
@@ -69,7 +69,6 @@ y = train['Transported']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 from sklearn.ensemble import HistGradientBoostingClassifier
-from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score
 hgbc = HistGradientBoostingClassifier()
 hgbc.fit(X_train,y_train)
